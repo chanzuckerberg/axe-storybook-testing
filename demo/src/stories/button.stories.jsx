ced5ae41e902a2e3608abe482136bd5c235ccd2a
Andrew Huth <ahuth@chanzuckerberg.com>
2020-08-25 11:13:25 -0700

    Update demo to have examples pertinent to axe-storybook

---
+++ demo/src/stories/button.stories.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Button({ children, ...rest }) {
+  return <button {...rest}>{children}</button>;
+}
+
+export default {
+  title: "button",
+  component: Button,
+};
+
+export const Button1 = () => <Button>Click me!</Button>
+Button1.storyName = "Button with text (should pass)";
+
+export const Button2 = () => <Button aria-label="Click me!">❤️</Button>;
+Button2.storyName = "Button with emoji and label (should pass)";
+
+export const Button3 = () => <Button>❤️</Button>;
+Button3.storyName = "Button with emoji but no label (should fail)";
+
+export const Button4 = () => <Button style={{ backgroundColor: "red", color: "hotpink"}}>Click me!</Button>
+Button4.storyName = "Button with insufficient color contrast (should fail)";
+
+export const Button5 = () => <Button style={{ backgroundColor: "red", color: "hotpink" }}>Click me!</Button>
+Button5.storyName = "Button with insufficient color contrast but skipped (should pass)";
+Button5.parameters = {
+  axe: {
+    skip: true,
+  },
+};
---
+++ demo/src/stories/input.stories.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Input(props) {
+  return <input {...props} />;
+}
+
+export default {
+  title: "input",
+  component: Input,
+};
+
+export const Input1 = () => (
+  <label>
+    Favorite coffee
+    <Input />
+  </label>
+)
+Input1.storyName = "Input with label (should pass)";
+
+export const Input2 = () => <Input placeholder="Favorite coffee" />;
+Input2.storyName = "Input without label (should fail)";
+
+export const Input3 = () => <Input placeholder="Favorite coffee" />;
+Input3.storyName = "Input without label but skipped (should pass)";
+Input3.parameters = {
+  axe: {
+    skip: true,
+  },
+};
