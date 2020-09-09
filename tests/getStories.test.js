ad4792bf882032b76d5993ae11dde07d60984124
Andrew Huth <ahuth@chanzuckerberg.com>
2020-09-09 10:20:16 -0700

    Simplify no stories test

--- tests/getStories.test.js
+++ tests/getStories.test.js
@@ -1,15 +1,9 @@
 import getStories from '../src/getStories';
 
 it('raises an error when no stories loaded', async () => {
-  try {
-    await getStories({ iframePath: __dirname + '/iframe-without-stories.html' });
-  } catch (e) {
-    const message =
-      'Evaluation failed: Error: Storybook object not found on window. Open your storybook and check the console for errors.';
-    expect(e.message.startsWith(message)).toEqual(true);
-  }
-
-  expect.assertions(1);
+  await expect(getStories({ iframePath: __dirname + '/iframe-without-stories.html' }))
+    .rejects
+    .toThrow(/Storybook object not found on window/);
 }, 30000);
 
 it('returns the stories from the window', async () => {
