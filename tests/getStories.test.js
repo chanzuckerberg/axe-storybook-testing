5a990c5ee113fbd734f2d0b8b5cbc85276df7b51
Andrew Huth <ahuth@chanzuckerberg.com>
2020-09-09 10:13:33 -0700

    Set test timeouts the Jest way

--- tests/getStories.test.js
+++ tests/getStories.test.js
@@ -1,16 +1,5 @@
 import getStories from '../src/getStories';
 
-let originalTimeout = 0;
-
-beforeEach(function() {
-  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
-});
-
-afterEach(function() {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-});
-
 it('raises an error when no stories loaded', async () => {
   try {
     await getStories({ iframePath: __dirname + '/iframe-without-stories.html' });
@@ -21,11 +10,11 @@ it('raises an error when no stories loaded', async () => {
   }
 
   expect.assertions(1);
-});
+}, 30000);
 
 it('returns the stories from the window', async () => {
   const stories = await getStories({ iframePath: __dirname + '/iframe.html' });
   expect(stories).toEqual([
     { kind: "this is the story's kind", name: "this is the story's name", parameters: {} },
   ]);
-});
+}, 30000);
