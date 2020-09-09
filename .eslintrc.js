8c555a484e77be62f35cfd39a759570d21ebb226
Andrew Huth <ahuth@chanzuckerberg.com>
2020-09-09 11:32:44 -0700

    Add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
@@ -13,6 +13,7 @@ module.exports = {
   plugins: ['@babel'],
   ignorePatterns: ['storybook-static/', 'build/'],
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'comma-dangle': ['error', 'always-multiline'],
     semi: ['error', 'always'],
     quotes: ['error', 'single', { avoidEscape: true }],
