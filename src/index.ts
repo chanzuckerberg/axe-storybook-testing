9bf8bd312e83867d68c0664db3a1aa083470d643
Andrew Huth <ahuth@chanzuckerberg.com>
2020-09-11 09:30:34 -0700

    Document each step via comments

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,23 @@ export async function run() {
   const options = parseOptions(debug.enabled);
   debug.enabled = options.debug;
 
+  // Get Storybook's representation of stories from its static iframe.
   const rawStories = await getRawStories(options.iframePath);
   debug('rawStories %s', JSON.stringify(rawStories));
 
+  // Process each story into a format that's more useful for us.
   const processedStories = getProcessedStories(rawStories);
   debug('processedStories %o', processedStories);
 
+  // Filter out stories that have disabled our integration.
   const selectedStories = selectStories(processedStories);
   debug('selectedStories %o', selectedStories);
 
+  // Visit each selected story and run Axe on them.
   const results = await getResults(selectedStories, options.iframePath);
   debug('results %o', results);
 
+  // Print the results in a human-readable way.
   display(results);
 
   return isPassing(results) ? Promise.resolve() : Promise.reject();
