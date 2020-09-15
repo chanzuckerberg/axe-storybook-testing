bde65f8c1ff92baed3c2385a16cdd7400381c5b0
Andrew Huth <ahuth@chanzuckerberg.com>
2020-09-15 08:49:10 -0700

    Don't write the top-level `describe` until after we have all the stories
    That way, Mocha "sees" the top-level describe and displays its name.

--- src/Suite.ts
+++ src/Suite.ts
@@ -8,32 +8,36 @@ import * as Result from './Result';
 
 const options = Options.parse();
 
-describe(`[${options.browser}] accessibility tests`, async function () {
+async function writeTests() {
   const browser = await playwright[options.browser].launch({ headless: options.headless });
   const context = await browser.newContext({ bypassCSP: true });
   const page = await context.newPage();
   const rawStories = await RawStory.fromIframe(options.iframePath, page);
   const processedStories = ProcessedStory.fromRawStories(rawStories);
 
-  after(async function () {
-    await browser.close();
-  });
+  describe(`[${options.browser}] accessibility`, function () {
+    after(async function () {
+      await browser.close();
+    });
 
-  processedStories.forEach((story) => {
-    const testFn = ProcessedStory.isEnabled(story) ? it : it.skip;
+    processedStories.forEach((story) => {
+      const testFn = ProcessedStory.isEnabled(story) ? it : it.skip;
 
-    testFn(story.name, async function () {
-      const result = await Result.fromStory(story, page, options.iframePath);
+      testFn(story.name, async function () {
+        const result = await Result.fromStory(story, page, options.iframePath);
 
-      if (result.violations.length === 0) {
-        assert.ok('Nice');
-      } else {
-        assert.fail('\n' + Result.formatViolations(result));
-      }
+        if (result.violations.length === 0) {
+          assert.ok('Nice');
+        } else {
+          assert.fail('\n' + Result.formatViolations(result));
+        }
+      });
     });
   });
 
   // Magic function injected by Mocha, signalling that the tests have been defined and are ready to
   // be ran. Only works because `delay: true` is passed to the Mocha constructor in index.ts.
   run();
-});
+}
+
+writeTests();
