297bc70410e1e622620e226dc515664214bcc53c
Eoin O'Brien <eoobrien@microsoft.com>
2020-11-02 20:45:25 +0000

    update gitignore

--- src/SarifOutputter.ts
+++ src/SarifOutputter.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 import { promisify } from 'util';
 
 /**
- * Outputs results from axe to a file in the Sarif format
+ * Outputs results from axe to a file in the SARIF format
  * @param sarifFileName the name of the file that will contain axeResults
  * @param axeResults results to output
  */
