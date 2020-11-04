61cf2c34f551d458368ed1fd221c3daf6066336f
Eoin O'Brien <eoobrien@microsoft.com>
2020-11-04 19:13:08 +0000

    Update gitignore, and use correct type

--- src/SarifOutputter.ts
+++ src/SarifOutputter.ts
@@ -1,15 +1,15 @@
 import options from './Options';
+import { AxeResults } from 'axe-core';
 import { convertAxeToSarif } from 'axe-sarif-converter';
 import * as fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
-
 /**
  * Outputs results from axe to a file in the SARIF format
  * @param sarifFileName the name of the file that will contain axeResults
  * @param axeResults results to output
  */
-export async function exportAxeAsSarifTestResult(sarifFileName: string, axeResults: any) {
+export async function exportAxeAsSarifTestResult(sarifFileName: string, axeResults: AxeResults) {
   if(options.outputSarifFiles)
   {
     const sarifResults = convertAxeToSarif(axeResults);
