99106a99239d5bf591676225c48ee5ac1ca3443d
Andrew Huth <ahuth@chanzuckerberg.com>
2020-11-12 13:42:05 -0800

    Add import linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,21 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
   ignorePatterns: ['storybook-static/', 'build/'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
