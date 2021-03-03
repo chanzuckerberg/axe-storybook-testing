b0408fed5ea8d98c0f5b4e33c0479c4509ad47af
Andrew Huth <ahuth@chanzuckerberg.com>
2021-03-03 14:24:40 -0500

    Turn on bugfixes in preset-env

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
   presets: [
     '@babel/preset-typescript',
     ['@babel/preset-env', {
+      // Transform syntax issues in buggy implemenetations, instead of providing polyfills in those
+      // cases. See https://babeljs.io/blog/2020/03/16/7.9.0#babelpreset-envs-bugfixes-option-11083httpsgithubcombabelbabelpull11083.
+      bugfixes: true,
       // Turn on "shipped proposals", which gives us access to non-stage4 proposals that have
       // nevertheless shipped in multiple browsers. The main proposal we need support for is
       // public class properties.
