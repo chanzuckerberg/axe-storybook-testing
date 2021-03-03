7d392121294239d3189d99ad8686fde68fe5394f
Andrew Huth <ahuth@chanzuckerberg.com>
2021-03-03 13:55:53 -0500

    Turn on support for shipped proposals

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    '@babel/preset-env',
+    ['@babel/preset-env', {
+      // Turn on "shipped proposals", which gives us access to non-stage4 proposals that have
+      // nevertheless shipped in multiple browsers. The main proposal we need support for is
+      // public class properties.
+      // See https://babeljs.io/docs/en/babel-preset-env#shippedproposals.
+      shippedProposals: true,
+    }],
   ],
   overrides: [
     {
