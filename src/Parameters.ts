fa90020688ffdcf1331102ccf4622d4630fcca95
Andrew Huth <ahuth@chanzuckerberg.com>
2021-09-24 14:42:11 -0700

    Remove unnecessary ? from arguments
    "unknown" already implies the possibility of "undefined".

--- src/Parameters.ts
+++ src/Parameters.ts
@@ -6,14 +6,14 @@ const skipSchema = zod.optional(zod.boolean());
 const disabledRulesSchema = zod.optional(zod.array(zod.string()));
 const waitForSelectorSchema = zod.optional(zod.string());
 
-export function parseSkip(skipped?: unknown): zod.infer<typeof skipSchema> {
+export function parseSkip(skipped: unknown): zod.infer<typeof skipSchema> {
   return skipSchema.parse(skipped);
 }
 
-export function parseDisabledRules(disabledRules?: unknown): zod.infer<typeof disabledRulesSchema> {
+export function parseDisabledRules(disabledRules: unknown): zod.infer<typeof disabledRulesSchema> {
   return disabledRulesSchema.parse(disabledRules);
 }
 
-export function parseWaitForSelector(waitForSelector?: unknown): zod.infer<typeof waitForSelectorSchema> {
+export function parseWaitForSelector(waitForSelector: unknown): zod.infer<typeof waitForSelectorSchema> {
   return waitForSelectorSchema.parse(waitForSelector);
 }
