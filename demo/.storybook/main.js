ad5a7cf58a6f0713841f28d2eb017be27badeeb9
Andrew Huth <ahuth@chanzuckerberg.com>
2021-11-04 17:02:16 -0700

    Convert demo examples to CSF v3

--- demo/.storybook/main.js
+++ demo/.storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  "stories": ["../src"],
   "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
--- demo/src/simple.stories.jsx
+++ demo/src/simple.stories.jsx
@@ -5,32 +5,42 @@ export default {
   component: 'button',
 };
 
-export const NoFailures = () => <button>hello world</button>;
-export const FailureNoDiscernibleText = () => <button></button>;
-export const FailureColorContrast = () => <button style={{ backgroundColor: 'red', color: 'hotpink' }}>hello world</button>;
-export const FailureNoDiscernibleTextAndInvalidRole = () => <button role="wut-the-wut"></button>;
+export const NoFailures = { render: () => <button>hello world</button> };
+export const FailureNoDiscernibleText = { render: () => <button></button> };
+export const FailureColorContrast = { render: () => <button style={{ backgroundColor: 'red', color: 'hotpink' }}>hello world</button> };
+export const FailureNoDiscernibleTextAndInvalidRole = { render: () => <button role="wut-the-wut"></button> };
 
-export const FailureColorContrastSkipped = FailureColorContrast.bind(null);
-FailureColorContrastSkipped.parameters = {
-  axe: { skip: true },
+export const FailureColorContrastSkipped = {
+  ...FailureColorContrast,
+  parameters: {
+    axe: { skip: true },
+  },
 };
 
-export const FailureNoDiscernibleTextAndInvalidRoleSkipped = FailureNoDiscernibleTextAndInvalidRole.bind(null);
-FailureNoDiscernibleTextAndInvalidRoleSkipped.parameters = {
-  axe: { skip: true },
+export const FailureNoDiscernibleTextAndInvalidRoleSkipped = {
+  ...FailureNoDiscernibleTextAndInvalidRole,
+  parameters: {
+    axe: { skip: true },
+  },
 };
 
-export const FailureColorContrastDisabledRule = FailureColorContrast.bind(null);
-FailureColorContrastDisabledRule.parameters = {
-  axe: { disabledRules: ['color-contrast'] },
+export const FailureColorContrastDisabledRule = {
+  ...FailureColorContrast,
+  parameters: {
+    axe: { disabledRules: ['color-contrast'] },
+  },
 };
 
-export const FailureNoDiscernibleTextAndInvalidRoleDisabledOneRule = FailureNoDiscernibleTextAndInvalidRole.bind(null);
-FailureNoDiscernibleTextAndInvalidRoleDisabledOneRule.parameters = {
-  axe: { disabledRules: ['aria-roles'] },
+export const FailureNoDiscernibleTextAndInvalidRoleDisabledOneRule = {
+  ...FailureNoDiscernibleTextAndInvalidRole,
+  parameters: {
+    axe: { disabledRules: ['aria-roles'] },
+  },
 };
 
-export const FailureNoDiscernibleTextAndInvalidRoleDisabledRules = FailureNoDiscernibleTextAndInvalidRole.bind(null);
-FailureNoDiscernibleTextAndInvalidRoleDisabledRules.parameters = {
-  axe: { disabledRules: ['aria-roles', 'button-name'] },
+export const FailureNoDiscernibleTextAndInvalidRoleDisabledRules = {
+  ...FailureNoDiscernibleTextAndInvalidRole,
+  parameters: {
+    axe: { disabledRules: ['aria-roles', 'button-name'] },
+  },
 };
