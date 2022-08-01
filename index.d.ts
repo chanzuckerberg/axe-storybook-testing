e5698e0b7409ec6610c76d04e3fd9055f8e07c76
Timothy Chang <timothy@chanzuckerberg.com>
2022-08-01 21:10:18 +0000

    fix lint

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 export declare type AxeParams = {
-    /**
-     * Prevents axe-storybook-testing from running Axe rules on this story.
-     */
-    skip?: boolean;
-    /**
-     * Prevents axe-storybook-testing from running specific Axe rules on this story.
-     * NOTE: array elements should be the names of axe-core rules found in
-     * https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md.
-     */
-    disabledRules?: string[];
-    /**
-     * Overrides the global timeout for this specific test (in ms)
-     */
-    timeout?: number;
-    /**
-     * @deprecated
-     * Legacy way of waiting for a selector before running Axe.
-     */
-    waitForSelector?: string;
+  /**
+   * Prevents axe-storybook-testing from running Axe rules on this story.
+   */
+  skip?: boolean;
+  /**
+   * Prevents axe-storybook-testing from running specific Axe rules on this story.
+   * NOTE: array elements should be the names of axe-core rules found in
+   * https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md.
+   */
+  disabledRules?: string[];
+  /**
+   * Overrides the global timeout for this specific test (in ms)
+   */
+  timeout?: number;
+  /**
+   * @deprecated
+   * Legacy way of waiting for a selector before running Axe.
+   */
+  waitForSelector?: string;
 };
