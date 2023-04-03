5abe464a77c5116028a5e2441bbef0d533b9902e
Andrew Huth <ahuth@chanzuckerberg.com>
2023-04-03 19:02:57 -0400

    Auto-migrate demo Storybook to v7 config

--- demo/.storybook/main.js
+++ demo/.storybook/main.js
@@ -1,14 +1,14 @@
 module.exports = {
   stories: ["../src"],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@storybook/addon-postcss",
-  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y", "@storybook/addon-postcss"],
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   core: {
-    disableTelemetry: true,
+    disableTelemetry: true
   },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  }
 };
