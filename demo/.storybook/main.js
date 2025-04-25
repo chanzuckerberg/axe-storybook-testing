2b82a4ec525b46abf27bc023613c99242946fc9c
Andrew Huth <ahuth@chanzuckerberg.com>
2025-04-25 16:48:51 -0400

    Update deps (#108)
    * Upgrade React to 19.1.0
    * Update Storybook to 8.6.12
    * Update vitest to 3.1.2
    * Update mocha to 11.1.0
    * Use Node 22.15.0 for local dev
    * Re-generate snapshots for the updated version of axe-core
    * Add changelog entry

--- demo/.storybook/main.js
+++ demo/.storybook/main.js
@@ -12,13 +12,4 @@ module.exports = {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  swc: () => ({
-    jsc: {
-      transform: {
-        react: {
-          runtime: 'automatic',
-        },
-      },
-    },
-  }),
 };
